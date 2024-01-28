94e164c29406c4f62cc544dd2b825fa9ef1c33e1
Nikita <cherenkov204@gmail.com>
2024-01-28 06:44:22 +0300

    one-hot without pandas.get_dummies

--- program.py
+++ program.py
@@ -1,6 +1,20 @@
+import pandas as pd
 import random
+
 lst = ['robot'] * 10
 lst += ['human'] * 10
 random.shuffle(lst)
 data = pd.DataFrame({'whoAmI':lst})
-data.head()
+print(data.head(n = 20))
+
+human = []
+robot = []
+for el in lst:
+    if el == 'robot':
+        robot.append(1)
+        human.append(0)
+    if el == 'human':
+        robot.append(0)
+        human.append(1)
+data = pd.DataFrame({'whoAmI_human': human, 'whoAmI_robot': robot})
+print(data.head(n = 20))
